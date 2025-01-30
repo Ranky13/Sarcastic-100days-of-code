76980d4920ef811ab1994445b005779ea8f70159
BASHIR ROKEEB <169052648+Ranky13@users.noreply.github.com>
2025-01-30 13:24:50 -0700

    Added quantity interactions

---
+++ kick'palace website/puma.js
@@ -0,0 +1,10 @@
+function updateQuantity(action, quantityId){
+    const quantityDisplay = document.getElementById(quantityId);
+    let quantity = parseInt(quantityDisplay.textContent);
+    if(action === `decrement` && quantity > 0) {
+        quantity--;
+    }else if(action === `increment`) {
+        quantity++;
+    }
+    quantityDisplay.textContent = quantity;
+}
--- kick'palace website/script.js
+++ kick'palace website/script.js
@@ -1,4 +1,10 @@
-const rokeeb = "run the environment";
-
-console.log(rokeeb);
-
+function updateQuantity(action, quantityId){
+    const quantityDisplay = document.getElementById(quantityId);
+    let quantity = parseInt(quantityDisplay.textContent);
+    if(action === `decrement` && quantity > 0) {
+        quantity--;
+    }else if(action === `increment`) {
+        quantity++;
+    }
+    quantityDisplay.textContent = quantity;
+}

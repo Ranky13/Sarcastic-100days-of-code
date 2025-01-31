c3ebfb30f2ba86dc14109df5ee4d9340ab03ec60
BASHIR ROKEEB <169052648+Ranky13@users.noreply.github.com>
2025-01-31 10:24:56 -0700

    updated the website

--- kick'palace website/nike.js
+++ kick'palace website/nike.js
@@ -12,3 +12,16 @@ function incrementQuantity() {
     quantity++;
     document.getElementById("quantity").textContent = quantity;
 }
+
+
+/*
+function updateQuantity(action, quantityId){
+    const quantityDisplay = document.getElementById(quantityId);
+    let quantity = parseInt(quantityDisplay.textContent);
+    if(action === `decrement` && quantity > 0) {
+        quantity--;
+    }else if(action === `increment`) {
+        quantity++;
+    }
+    quantityDisplay.textContent = quantity;
+}

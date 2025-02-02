9b9aeac8aabdc0061c9d18f44a09da1eba147df8
BASHIR ROKEEB <169052648+Ranky13@users.noreply.github.com>
2025-02-02 14:31:47 -0700

    Add files via upload

--- kick'palace website/NewBalance.js
+++ kick'palace website/NewBalance.js
@@ -9,6 +9,7 @@ function updateQuantity(action, quantityId){
     quantityDisplay.textContent = quantity;
 };
 
+
 //? WishList Scripts----------------------------------------------------------------------------------------------------------------------------//
 
 const heartIcons = document.querySelectorAll(`.bi-heart`);
@@ -17,7 +18,6 @@ heartIcons.forEach((icon) => {
     icon.addEventListener(`click`, () => {
         icon.classList.toggle(`bi-heart-fill`);
         icon.classList.toggle(`bi-heart`);
-
         if (icon.classList.contains(`bi-heart-fill`)){
             alert(`added to wishlisst`);
         }else{
@@ -31,10 +31,49 @@ heartIcons.forEach((icon) => {
 //? variants Scripts----------------------------------------------------------------------------------------------------------------------
 
 const ThumbImage = document.querySelectorAll(`.ThumbImg`);
-const variantsImage = document.querySelectorAll(`.variantImg`);
+const variantsImage = document.querySelectorAll(`.variantImg`); 
 
 variantsImage.forEach((variant) => {
     variant.addEventListener(`click`, () => {
         ThumbImage.src = variant.src;
     });
 });
+
+
+//? cart script--------------------------------------------------------------------------------------------------------------------------
+
+const addToCartButton = document.getElementById(`add-to-cart`);
+const SneakerName = document.getElementById(`sneaker-name`);
+const ProductDescription = document.getElementById(`description`);
+const SneakerPrice = document.getElementById(`price`);
+const ProductTag = document.getElementById(`tag`);
+const selectedQuantity = document.querySelector(`.quantity-display`).textContent;
+
+addToCartButton.addEventListener(`click`, () => {
+    const cartItem = {
+        name: SneakerName,
+        description: ProductDescription;
+        price: SneakerPrice,
+        tag: ProductTag;
+        quantity: selectedQuantity
+    };
+
+    const existingCartItems = localStorage.getItem(`cartItem`);
+
+    if (existingCartItems) {
+        const cartItem = JSON.parse(existingCartItems);
+        cartItem.push(cartItem);
+        localStorage.setItem(`cartItem`, JSON.stringify(cartItem));
+    }else {
+        const cartItem = [cartItem];
+        localStorage.setItem(`cartItem`,JSON.stringify(cartItem));
+    }
+    updateCartCount();
+    });
+
+    function updateCartCount() {
+        const cartItem = JSON.parse(localStorage.getItem(`cartItem`));
+        const cartCount = cartItem.textContent;
+        document.getElementById(`cart-count`).textContent = cartCount;
+
+    };
